76acda4d8156a8fef083385bf0b91713e9f918a7
Joanna Chan <joanna.chan@live.ca>
2019-05-30 19:04:33 -0400

    Added an api to get all conferences (#1)

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
 const { healthRouter } = require('../routes/health/health.router')
-
+const { conferencesRouter } = require('../routes/conferences/conferences.router');
 const router = express.Router();
 router.use("/health", healthRouter);
-
+router.use("/conferences", conferencesRouter);
 module.exports = router;
