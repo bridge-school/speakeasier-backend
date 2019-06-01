205823c867ceb5819edff0e9db70dc3e21233403
Nayema Nasir <nayema.nasir89@gmail.com>
2019-06-01 11:57:07 -0400

    Feat/post conference (#4)
    * Add express json parsing
    
    * Remove console logs

--- src/routes/conferences/conferences.controller.js
+++ src/routes/conferences/conferences.controller.js
@@ -18,6 +18,24 @@ const getConferencesController = (req, res) => {
 	        });
 }
 
+const postConferenceController = (req, res) => {
+	db.collection("conferences")
+		.add({
+			...req.body
+		})
+		.then(docRef => {
+			res.status(201)
+				.json({
+					id: docRef.id,
+					message: "Conference successfully created"
+				});
+		})
+		.catch(error => {
+			res.json({ error });
+		});
+};
+
 module.exports = {
-  getConferencesController
+  getConferencesController,
+	postConferenceController
 }
