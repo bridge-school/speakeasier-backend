a2f456cb9269bfee888184e29a321b2d06774344
Vanessa Mendoza <vanessamendoza@Vanessas-MacBook-Pro.local>
2019-06-01 17:20:32 -0400

    Returned doc after saveing conference

--- src/routes/conferences/conferences.controller.js
+++ src/routes/conferences/conferences.controller.js
@@ -2,32 +2,31 @@ const db = require('../../db');
 
 const getConferencesController = (req, res) => {
 	db.collection("conferences")
-	        .get()
-	        .then(snapshot => {
-	            res.json({
-	                data: snapshot.docs.map(doc => {
-	                    return {
-	                        id: doc.id,
-	                        ...doc.data()
-	                    };
-	                })
-	            });
-	        })
-	        .catch(error => {
-	            res.json({ error });
-	        });
+    .get()
+    .then(snapshot => {
+      res.json({
+        data: snapshot.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          };
+        })
+      });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
 }
 
 const postConferenceController = (req, res) => {
 	db.collection("conferences")
-		.add({
-			...req.body
-		})
-		.then(docRef => {
+		.add(req.body)
+		.then(docRef => docRef.get())
+		.then(doc => {
 			res.status(201)
 				.json({
-					id: docRef.id,
-					message: "Conference successfully created"
+					id: doc.id,
+					...doc.data()
 				});
 		})
 		.catch(error => {
