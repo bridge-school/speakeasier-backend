cbfa748ddb482621c9a6ebe11fcd81fe8d63387a
Nayema Nasir <nayema.nasir89@gmail.com>
2019-06-17 02:30:49 -0400

    Rearrange index

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,7 @@
-const express = require('express');
-
+const express = require("express");
 const { healthRouter } = require('../routes/health/health.router');
 const { conferencesRouter } = require('../routes/conferences/conferences.router');
 const router = express.Router();
-
-router.use('/health', healthRouter);
-router.use('/conferences', conferencesRouter);
-
+router.use("/health", healthRouter);
+router.use("/conferences", conferencesRouter);
 module.exports = router;
